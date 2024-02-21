eb7bbbf5248acce05ecf16a9d2d7f8def5401c98
samielsabri <sami.elsabri@mail.utoronto.ca>
2024-02-21 13:48:37 -0500

    refactored some files

--- domain/use_cases/analyze_languages.py
+++
@@ -1,6 +0,0 @@
-class AnalyzeLanguages:
-    def __init__(self, language_detector):
-        self.language_detector = language_detector
-
-    def execute(self, lyrics):
-        return self.language_detector.detect_languages(lyrics)
